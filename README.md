ccd7c0a2a87932e953cc31bc718c2aff5372f70f
Ben Leitch <benleitch17@gmail.com>
2025-08-11 22:08:48 -0700

    Iron out the blob selection

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Gamelan-Frequencies
+
 Using signal processing to investigate the tuning / frequencies of various Gamelan instruments.
+
+## Environment Setup
+
+Using Python `13.5`
+
+TODO - finish complete README once code is working.
--- analyze.py
+++ analyze.py
@@ -24,58 +24,69 @@ def is_region_nan(data: npt.NDArray[np.float64], region_start_index: int, region
     return bool(np.all(np.isnan(data[region_start_index:region_start_index+region_length_samples])))
 
 
-def create_not_nan_blob_mask(data_with_nan: npt.NDArray[np.float64], blob_length_s: float, sample_rate_hz: float) -> npt.NDArray[np.bool]:
+def create_non_nan_region_mask(data_with_nan: npt.NDArray[np.float64], region_length_s: float, sample_rate_hz: float) -> npt.NDArray[np.bool]:
     """
-    Create a boolean mask array to identify continuous regions of data (aka "blobs") that are not all nan within the input array.
-    If mask[i] == True, then data_with_nan[i] belongs to a blob, even if data_with_nan[i] is nan.
+    Create a boolean mask array to identify continuous regions of data that are not all nan within the input array.
+    If mask[i] == True, then index i belongs to a non-nan region, even if data_with_nan[i] is nan.
 
     Return the boolean mask array.
     """
     
-    blob_mask: npt.NDArray[np.bool] = np.zeros_like(data_with_nan).astype(np.bool)
+    non_nan_region_mask: npt.NDArray[np.bool] = np.zeros_like(data_with_nan).astype(np.bool)
     
-    blob_length_samples: int = math.floor(blob_length_s * sample_rate_hz)
+    region_length_samples: int = math.floor(region_length_s * sample_rate_hz)
 
-    for index in range(0, len(data_with_nan) - blob_length_samples, blob_length_samples):
-        if not is_region_nan(data_with_nan, index, blob_length_samples):
-            blob_mask[index:index+blob_length_samples] = True
+    for index in range(0, len(data_with_nan) - region_length_samples, region_length_samples):
+        if not is_region_nan(data_with_nan, index, region_length_samples):
+            non_nan_region_mask[index:index+region_length_samples] = True
 
-    return blob_mask
+    return non_nan_region_mask
 
 
-def get_region_boundaries(region_mask: npt.NDArray[np.bool], min_blob_size_s: float, sample_rate_hz: float) -> list[tuple[int, int]]:
+def get_blob_boundaries(non_nan_region_mask: npt.NDArray[np.bool], min_blob_size_s: float, sample_rate_hz: float) -> list[tuple[int, int]]:
     """
-    Given a boolean mask where mask[i] == True indicates that i falls within a 
+    A blob is defined as a continuous set of samples that are assumed to contain useful data, as opposed to noise.
+    
+    This function accepts a boolean mask non_nan_region_mask where non_nan_region_mask[i] == True indicates that index i refers to a sample
+    that falls in a "non-nan" region that may potentially contain useful data.
+
+    This function returns pairs of inclusive blob boundary indices (start, end) such that the size of each blob is at least min_blob_size_s
+    in terms of samples. 
     """
     
     min_blob_size_samples: int = math.floor(min_blob_size_s * sample_rate_hz)
-    
-    # Compute the difference between adjacent elements
-    diff = np.diff(region_mask)
-    
-    # Starts of runs: where diff == 1 (0->1 transition), add +1 to get start index
-    run_starts = np.where(diff == 1)[0] + 1
-    
-    # Ends of runs: where diff == -1 (1->0 transition), index is end of run
-    run_ends = np.where(diff == -1)[0]
-    
-    # Handle if the array starts with a run
-    if region_mask[0] == 1:
-        run_starts = np.insert(run_starts, 0, 0)
-    # Handle if the array ends with a run
-    if region_mask[-1] == 1:
-        run_ends = np.append(run_ends, len(region_mask) - 1)
-    
-    # Return list of (start, end) pairs (inclusive)
-    runs: list[tuple[int, int]] = list(zip(run_starts, run_ends))
 
-    return [(int(start), int(end)) for start, end in filter(lambda indices: indices[-1] - indices[0] > min_run_size, runs)]
+    # From the numpy docs: https://numpy.org/doc/stable/reference/generated/numpy.diff.html
+    # The first difference is given by out[i] = a[i + 1] - a[i].
+    # So in this case, it is the later index minus the earlier index.
+    # Therefore:
+    #   diff[i] == 1 -> mask[i + 1] - mask[i] == 1 -> mask[i + 1] == 1, and mask[i] == 0. Since mask[i + 1] == 1, i + 1 is the first index of the region.
+    #   diff[i] == -1 -> mask[i + 1] - mask[i] == -1 -> mask[i + 1] == 0, and mask[i] == 1. Since mask[i] == 1, i is the last index of the region. 
+    # Important: need to cast non_nan_region_mask to int8 before performing the subtraction.
+    non_nan_region_mask_diff: npt.NDArray[np.int8] = np.diff(non_nan_region_mask.astype(np.int8))
+
+    # Need to add 1 to get the proper index based on above.
+    blob_starts: npt.NDArray[np.intp] = np.where(non_nan_region_mask_diff == 1)[0] + 1
+
+    # If the non_nan_region_mask starts with a 1, then treat index 0 as the start of a potential blob. 
+    if non_nan_region_mask[0] == 1:
+        blob_starts = np.insert(blob_starts, 0, 0)
+
+    blob_ends: npt.NDArray[np.intp] = np.where(non_nan_region_mask_diff == -1)[0]
+
+    if non_nan_region_mask[-1] == 1:
+        blob_ends = np.append(blob_ends, len(non_nan_region_mask) - 1)
+    
+    blob_boundaries: list[tuple[int, int]] = list(zip(blob_starts, blob_ends))
+
+    # Filter for blobs larger than the provided min_blob_size.
+    return [(int(start), int(end)) for start, end in filter(lambda indices: indices[-1] - indices[0] > min_blob_size_samples, blob_boundaries)]
 
 
-def create_selected_audio_data_array(audio_data: npt.NDArray[np.float64], run_indices: list[tuple[int, int]]) -> npt.NDArray[np.float64]:
+def create_selected_audio_data_array(audio_data: npt.NDArray[np.float64], blob_boundaries: list[tuple[int, int]]) -> npt.NDArray[np.float64]:
     selected_mask: npt.NDArray[np.bool] = np.zeros_like(audio_data).astype(np.bool)
 
-    for start_index, end_index in runs:
+    for start_index, end_index in blob_boundaries:
         selected_mask[start_index:end_index+1] = True
 
     selected_audio_data: npt.NDArray[np.float64] = audio_data.copy()
@@ -107,12 +118,10 @@ if __name__ == "__main__":
     above_threshold_audio_data: npt.NDArray = audio_data.copy()
     above_threshold_audio_data[audio_data < THRESHOLD] = np.nan
 
-    # TODO - Pick up here, determine naming conventions, i.e.: region vs blob.
+    non_nan_region_mask: npt.NDArray[np.bool] = create_non_nan_region_mask(data_with_nan=above_threshold_audio_data, region_length_s=0.1, sample_rate_hz=sr)
 
-    region_mask: npt.NDArray[np.bool] = create_not_nan_blob_mask(data_with_nan=above_threshold_audio_data, blob_length_s=0.1, sample_rate_hz=sr)
-
-    min_run_size_samples: int = math.floor(0.5 * sr)
-    runs: list[tuple[int, int]] = get_region_boundaries(region_mask, min_run_size_samples)
+    blob_boundaries: list[tuple[int, int]] = get_blob_boundaries(non_nan_region_mask=non_nan_region_mask, min_blob_size_s=0.5, sample_rate_hz=sr)
 
     # Uncomment the line below to debug selection using the runs.
-    plot_selected_data(audio_data, create_selected_audio_data_array(audio_data, runs), sr)
+    # plot_selected_data(audio_data, create_selected_audio_data_array(audio_data, blob_boundaries), sr)
+
