d6ee78b6b03e845f85cdd658a992fae3d37496ba
Ben Leitch <benleitch17@gmail.com>
2025-09-07 20:46:08 -0700

    Trying to learn good habits for Python classes

--- metallophone.py
+++ metallophone.py
@@ -1,39 +1,127 @@
+from dataclasses import dataclass
+
 from note import Note
 
 
-class Metallophone:
+UPPER_PAIR_STR_SUFFIX: str = "+"
+LOWER_PAIR_STR_SUFFIX: str = "-"
+
+
+@dataclass(frozen=True)
+class GamelanMetallophoneLabel:
     """
-    A class to represent the Metallophone instrument used in Gamelan.
+    A class to represent a label format for the Metallophone instrument used in Gamelan music.
 
     Parameters
     ----------
     name:
-        The name of the Metallophone, e.g.: jegogan, kantilan, ugal, etc.
-    
-    wav_file_path:
-        A path to a .wav file containing key strikes for all the keys this instrument has.
-        These key strikes are expected to be ordered from lowest note (frequency) to highest note (frequency).
-        A particular key may be struck more than once in a row.
-        Each key strike is expected to last at least 0.5 seconds, and two different key strikes may not overlap.
+        The name of the instrument.
 
-    note_symbols:
-        A collection of note symbols corresponding to the notes represented by this instrument.
-        The symbols are expected to be ordered from lowest note to highest note, aligning with the order of the key
-        strikes in the recording .wav file.
+    is_upper:
+        True if the instrument is the upper metallophone of a tuned pair, otherwise False.
 
     is_lower:
-        If true, indicates this metallophone is the lower of a tuned pair of metallophones.
-    
+        True if the instrument is the lower metallophone of a tuned pair, otherwise False.
+
+    Attributes
+    ----------
+    name:
+        The name of the instrument.
+
     is_upper:
-        If true, indicates this metallophone is the upper of a tuned pair of metallophones.
+        True if the instrument is the upper metallophone of a tuned pair, otherwise False.
+
+    is_lower:
+        True if the instrument is the lower metallophone of a tuned pair, otherwise False.
+    """
+
+    name: str
+    is_upper: bool
+    is_lower: bool
+
+    def __init__(self, name: str, is_upper: bool, is_lower: bool):
+        if not name:
+            raise ValueError(f"{type(self).__name__}: Expected non-empty name.")
+        
+        if ' ' in name:
+            raise ValueError(f"{type(self).__name__}: A valid name should not include whitespace, instead got: {name!r}")
+
+        if is_upper and is_lower:
+            raise ValueError(f"{type(self).__name__}: A Metallophone cannot both be the upper and lower Metallophone of a tuned pair.")
+        
+        self.name = name
+        self.is_upper = is_upper
+        self.is_lower = is_lower
+
+    @classmethod
+    def from_string(cls, label_string: str) -> "GamelanMetallophoneLabel":
+        """
+        Create a GamelanMetallophoneLabel from a string representation.
+
+        Parameters
+        ----------
+        label_string:
+            The string representation of the label. Expected format is {name}{+ or - or empty}.
+            The + indicates this is the upper metallophone of a tuned pair, the - indicates this is the lower.
+            If no + or - is provided, then this is the leading instrument, not part of a tuned pair.
+
+        Returns
+        -------
+        GamelanMetallophoneLabel
+            The constructed GamelanMetallophoneLabel instance.
+        """
+        if len(label_string) <= 1:
+            raise ValueError(f"{cls.__name__}: A valid label string must have more than 1 character, instead got: {label_string!r}")
+
+        if ' ' in label_string:
+            raise ValueError(f"{cls.__name__}: A valid label string should not have whitespace, instead got: {label_string!r}")
+
+        if label_string[:-1].count(UPPER_PAIR_STR_SUFFIX) or label_string[:-1].count(LOWER_PAIR_STR_SUFFIX):
+            raise ValueError(f"{cls.__name__}: A valid label string should include at most one '+' or one '-' at the end of the string, instead got: {label_string!r}")
+
+        if label_string.endswith(UPPER_PAIR_STR_SUFFIX):
+            return cls(label_string[:-1], True, False)
+
+        elif label_string.endswith(LOWER_PAIR_STR_SUFFIX):
+            return cls(label_string[:-1], False, True)
+        
+        else:
+            return cls(label_string, False, False)
+
+
+@dataclass(frozen=True)
+class GamelanMetallophone:
+    """
+    A class to represent the Metallophone instrument used in Gamelan music.
+
+    Parameters
+    ----------
+    label:
+        The label of the Metallophone, e.g.: jegogan+, kantilan-, ugal, etc.
+    
+    notes:
+        The notes the instrument has.
 
     Attributes
     ----------
     name:
         The name of the Metallophone, e.g.: jegogan, kantilan, ugal, etc.
     
+    notes:
+        The notes the instrument has.
+
+    is_lower:
+        If true, indicates this metallophone is the lower of a tuned pair of metallophones.
     
+    is_upper:
+        If true, indicates this metallophone is the upper of a tuned pair of metallophones.
     """
 
-    def __init__(self, name: str, wav_file_path: str, note_symbols: tuple[str, ...], is_lower: bool, is_upper: bool) -> None:
-        pass
+    def __init__(self, label: GamelanMetallophoneLabel, notes: tuple[Note, ...]):
+        self.name: str = label.name
+        self.notes: tuple[Note, ...] = tuple([note for note in notes])
+        self.is_lower: bool = label.is_lower
+        self.is_upper: bool = label.is_upper
+
+    def __repr__(self) -> str:
+        return f"{type(self).__name__}(label={GamelanMetallophoneLabel(self.name, self.is_upper, self.is_lower)!r}, notes={self.notes!r})"
--- note.py
+++ note.py
@@ -2,6 +2,107 @@ from dataclasses import dataclass
 
 
 @dataclass(frozen=True)
+class NoteSymbol:
+    """
+    A class to represent a note symbol, e.g.: A_4, C#-3, etc.
+
+    Parameters
+    ----------
+    name:
+        The name of the note.
+
+    octave:
+        The octave the note belongs to, starting from 1.
+    
+    separator:
+        The separator string used in the string representation of the note symbol.
+        Defaults to "_".
+    
+    Attributes
+    ----------
+    name:
+        The name of the note.
+
+    octave:
+        The octave the note belongs to, starting from 1.
+    
+    separator:
+        The separator string used in the string representation of the note symbol.
+    """
+
+    name: str
+    octave: int
+    separator: str = "_"
+
+    def __init__(self, name: str, octave: int, separator: str = "_"):
+        if not name or not separator:
+            raise ValueError(f"{type(self).__name__}: Empty name: {name!r} or empty separator: {separator!r}")
+        
+        if ' ' in name:
+            raise ValueError(f"{type(self).__name__}: A valid name should not have whitespace, instead got: {name!r}")
+
+        if separator not in name:
+            raise ValueError(f"{type(self).__name__}: Separator {separator!r} not contained in name: {name!r}")
+
+        if name.count(separator) > 1:
+            raise ValueError(f"{type(self).__name__}: Expected separator {separator!r} to appear at most once in name: {name!r}")
+
+        if octave < 1:
+            raise ValueError(f"{type(self).__name__}: Octave must be at least 1, instead got: {octave!r}")
+        
+        self.name = name
+        self.octave = octave
+        self.separator = separator
+
+    def __str__(self) -> str:
+        return f"{self.name}{self.separator}{self.octave}"
+
+    @classmethod
+    def from_string(cls, note_string: str, separator: str = "_") -> "NoteSymbol":
+        """
+        Create a NoteSymbol instance from a string representation, e.g.: "A_4", "C#-3", etc.
+
+        Parameters
+        ----------
+        note_string:
+            The string representation of the note. Expected format is {name}{separator}{octave},
+            where name is any string, separator is any string, and octave is a positive integer.
+        
+        separator:
+            The separator used in the string representation of the note symbol. Should be the same
+            separator used in note_string.
+            Defaults to "_".
+
+        Returns
+        -------
+        NoteSymbol
+            The constructed NoteSymbol instance.
+        """
+        if not note_string or not separator:
+            raise ValueError(f"{cls.__name__}: Empty note_string: {note_string!r} or empty separator: {separator!r}")
+        
+        if separator not in note_string:
+            raise ValueError(f"{cls.__name__}: Separator {separator!r} not contained in note string: {note_string!r}")
+
+        note_symbol_segments: list[str] = note_string.split(separator)
+
+        if len(note_symbol_segments) != 2:
+            raise ValueError(f"{cls.__name__}: Expected separator {separator!r} to appear at most once in note string: {note_string!r}")
+        
+        name, octave = note_symbol_segments
+
+        if not octave.isdigit(octave):
+            raise ValueError(f"{cls.__name__}: Expected octave to be digits, instead got {octave!r}")
+        
+        if octave < 1:
+            raise ValueError(f"{cls.__name__}: Octave must be at least 1, instead got: {octave!r}")
+        
+        octave = int(octave)
+        
+        return cls(name, octave, separator)
+
+
+@dataclass(frozen=True)
 class Note:
     """
     A class for representing musical notes.
@@ -9,7 +110,7 @@ class Note:
     Parameters
     ----------
     symbol:
-        The sybol of the note. This symbol will be used as the string representation of the note.
+        The sybol of the note.
 
     freq_hz:
         The frequency, in Hz, of the tone the note represents.
@@ -17,18 +118,21 @@ class Note:
     Attributes
     ----------
     symbol:
-        The sybol of the note. This symbol will be used as the string representation of the note.
+        The sybol of the note.
 
-    freq:
+    freq_hz:
         The frequency, in Hz, of the tone the note represents.
     """
 
-    def __init__(self, symbol: str, freq_hz: float) -> None:
+    def __init__(self, symbol: NoteSymbol, freq_hz: float):
+        if freq_hz <= 0.0:
+            raise ValueError(f"{type(self).__name__}: Expected a frequency above 0 Hz, instead got: {freq_hz!r}")
+        
         self.symbol: str = symbol
-        self.freq: float = freq_hz
+        self.freq_hz: float = freq_hz
     
     def __str__(self) -> str:
-        return self.symbol
+        return str(self.symbol)
 
     def __repr__(self) -> str:
-        return f"Note(symbol={self.symbol}, freq={self.freq})"
+        return f"{type(self).__name__}(symbol={self.symbol!r}, freq_hz={self.freq_hz!r})"
